b4124923eb2cdb5aec2695aaa0b050220cb2d210
somama Ayan <somama.ayaan@gmail.com>
2026-09-17 16:10:59 +0500

    Re-add trust proxy setting for HAProxy

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import cookieParser from "cookie-parser"
 import rateLimit from "express-rate-limit"
 import helmet from "helmet"
 
-// for haproxy , 
-app.set('trust proxy', 1)
+
 
 import authRoutes from "./routes/auth.routes.js"
 import { errorHandler } from "./middleware/error.middleware.js"
 const app = express();
+
+// for haproxy , 
+app.set('trust proxy', 1)
 
 app.use(express.json({limit: "10kb"}));
 app.use(helmet());
